701affeabbe6cd35a249d5348ab2d47c17a08ba0
xhg-mxh <42367302+xhg-mxh@users.noreply.github.com>
2025-04-04 16:33:35 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -10,7 +10,7 @@ const CONFIG = {
 
   // 英雄区块导航
   STARTER_HERO_ENABLE: true, // 开启英雄区
-  STARTER_HERO_TITLE_1: '开源且免费的基于 Notion 笔记的网站构建工具', // 英雄区文字
+  STARTER_HERO_TITLE_1: '开源且免费的基于 Notion 笔记的网站构建工具冯冯', // 英雄区文字
   STARTER_HERO_TITLE_2: '通过笔记无感知地建站、成倍放大您的价值', // 英雄区文字
   // 英雄区两个按钮，如果TEXT留空则隐藏按钮
   STARTER_HERO_BUTTON_1_TEXT: '开始体验', // 英雄区按钮
